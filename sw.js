7780f0157f8906256b1fc6dd90360850e033b07e
ASIMKARD <johnjohn22122@outlook.com>
2026-08-01 17:10:24 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const FONTS = ['./fonts/anton-400-latin-ext.woff2', './fonts/anton-400-latin.woff2', './fonts/ibm-plex-mono-400-latin-ext.woff2', './fonts/ibm-plex-mono-400-latin.woff2', './fonts/ibm-plex-mono-500-latin-ext.woff2', './fonts/ibm-plex-mono-500-latin.woff2', './fonts/ibm-plex-sans-400-latin-ext.woff2', './fonts/ibm-plex-sans-400-latin.woff2', './fonts/ibm-plex-sans-500-latin-ext.woff2', './fonts/ibm-plex-sans-500-latin.woff2', './fonts/ibm-plex-sans-600-latin-ext.woff2', './fonts/ibm-plex-sans-600-latin.woff2'];
-const CACHE = 'spider-man-v7';
+const CACHE = 'spider-man-v8';
 const ASSETS = [
   './', './index.html', './styles.css', './data.js', './qrcode.js', './manifest.json',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable.png'
